bd796167167feb2c329d141ebad4c8623e8df012
Desmartin Antoine <desmartin.aantoine@gmail.com>
2023-07-28 12:30:31 +0200

    first commit of backend of Tendance with SetUp

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 require("../models/connection");
+require("../models/connection");
+
+// const User = require("../models/users");                      EXEMPLE
+// const Tweet = require("../models/tweets");                    EXEMPLE
+
+// const { checkBody } = require("../modules/checkBody");           Module
+
+
+// const uid2 = require("uid2");
+// const bcrypt = require("bcrypt");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
