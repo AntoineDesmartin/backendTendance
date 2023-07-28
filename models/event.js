29c1125125ea49bfbda23ca89a7b8ae373ca3c93
Desmartin Antoine <desmartin.aantoine@gmail.com>
2023-07-28 12:30:18 +0200

    first commit of backend of Tendance with SetUp

---
+++ models/event.js
@@ -0,0 +1,13 @@
+//! Exemple 
+// const mongoose = require('mongoose');
+
+// const MarkerSchema = mongoose.Schema({
+//     nickname: String,
+//     name: String,
+//     latitude: Number,
+//     longitude: Number,
+// });
+
+// const Marker = mongoose.model('markers', MarkerSchema);
+
+// module.exports = Marker;
